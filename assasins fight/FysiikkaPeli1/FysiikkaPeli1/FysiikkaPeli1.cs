8e47a1a3f5baf0114a139f8a5770dbdd1dcff4ea
EXIBEL <saku.tusa@gmail.com>
2013-11-14 08:27:20 -0800

    peli valmistuu hyvin

--- assasins fight/FysiikkaPeli1/FysiikkaPeli1/FysiikkaPeli1.cs
+++ assasins fight/FysiikkaPeli1/FysiikkaPeli1/FysiikkaPeli1.cs
@@ -10,43 +10,53 @@ public class FysiikkaPeli1 : PhysicsGame
 {
     PlatformCharacter2 Ukko;
     Image paaukko = LoadImage("paaukko");
-    public override void Begin()
+    PlatformCharacter vihollinen;
+    public override void Begin() 
     {
 
-       //Ukko = new PhysicsObject(50, 50);
-       //Ukko.Image = paaukko;
-       //Add(Ukko);
+        //Ukko = new PhysicsObject(50, 50);
+        //Ukko.Image = paaukko;
+        //Add(Ukko);
         Luokentta();
-    
+
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
-        Keyboard.Listen(Key.D, ButtonState.Pressed, Liikuta, "oikealle",Direction.Right);
-        Keyboard.Listen(Key.A, ButtonState.Pressed, Liikuta, "vasemmalle",Direction.Left);
-        Keyboard.Listen(Key.W, ButtonState.Pressed, Liikuta, "ylös",Direction.Up); 
-        Keyboard.Listen(Key.S, ButtonState.Pressed, Liikuta, "alas",Direction.Down);
-    } 
-    void Luokentta() {
+        Keyboard.Listen(Key.D, ButtonState.Pressed, Liikuta, "oikealle", Direction.Right);
+        Keyboard.Listen(Key.A, ButtonState.Pressed, Liikuta, "vasemmalle", Direction.Left);
+        Keyboard.Listen(Key.W, ButtonState.Pressed, Hyppaa, "ylös");
+        Keyboard.Listen(Key.S, ButtonState.Pressed, Liikuta, "alas", Direction.Down); 
+
     
+    }
+    void Hyppaa()
+    {
+        Ukko.Jump(500); 
+    }     
+    void Luokentta()
+    {
+
         ColorTileMap ruudut = ColorTileMap.FromLevelAsset("kentta");
 
         //2. Kerrotaan mitä aliohjelmaa kutsutaan, kun tietyn värinen pikseli tulee vastaan kuvatiedostossa.
-        ruudut.SetTileMethod(Color.FromHexCode("0094FF"),LuoPelaaja);
-        ruudut.SetTileMethod(Color.FromHexCode("7F3300"),LuoTaso);
-        ruudut.SetTileMethod(Color.FromHexCode("FF0000"),LuoVihollinen);
-        ruudut.SetTileMethod(Color.FromHexCode("808080"),LuoSeina);
+        ruudut.SetTileMethod(Color.FromHexCode("0094FF"), LuoPelaaja);
+        ruudut.SetTileMethod(Color.FromHexCode("7F3300"), LuoTaso);
+        ruudut.SetTileMethod(Color.FromHexCode("FF0000"), LuoVihollinen);
 
         //3. Execute luo kentän
         //   Parametreina leveys ja korkeus
-        ruudut.Execute(10, 10);
+        ruudut.Execute(50, 50);
     }
 
     void LuoPelaaja(Vector paikka, double leveys, double korkeus)
     {
-        Ukko = new PlatformCharacter2(20, 20);
+        Ukko = new PlatformCharacter2(50, 50);
         Ukko.Position = paikka;
         Ukko.Color = Color.FromHexCode("0094FF");
         Ukko.Image = LoadImage("paaukko");
         Add(Ukko);
+        Camera.Follow(Ukko);
+        Camera.ZoomFactor = 2; 
+        Gravity = new Vector(0, -1000);
     }
 
 
@@ -54,37 +64,27 @@ public class FysiikkaPeli1 : PhysicsGame
     {
         PhysicsObject taso = PhysicsObject.CreateStaticObject(leveys, korkeus);
         taso.Position = paikka;
-        //taso.Image = groundImage; 
         taso.Color = Color.FromHexCode("7F3300");
         taso.CollisionIgnoreGroup = 1; 
         Add(taso);
     }
-    
-    void LuoSeina(Vector paikka, double leveys, double korkeus)
-    {
-    PhysicsObject Seina = new PhysicsObject(80, 80);
-   
-    }
     void LuoVihollinen(Vector paikka, double leveys, double korkeus)  
     {
-    LuoVihollinen = vihollinen;
-    vihollinen = new PlatformCharacter(20, 20);
-
-    
-    
-    
+        vihollinen = new PlatformCharacter(20, 20);
+        vihollinen.Color = Color.FromHexCode("FF0000");
+        //vihollinen.Image = LoadImage("vihollinen");
+        Add(vihollinen);
     }
-    void Liikuta(Direction suunta) 
-    
+
+    void Liikuta(Direction suunta)
     {
         Ukko.Walk(suunta);
-      
-    
-    
+        Ukko.Velocity = new Vector(500, 0);
+
     }
 
 }
-
+ 
 
  
 
